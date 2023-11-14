99828beed703a7164bf64af54e7dc87ba6e90722
Ilyaskhamliche <113136909+Ilyaskhamliche@users.noreply.github.com>
2023-11-13 19:41:32 -0500

    Update M03 part 2,1.cpp

--- M03 part 2,1.cpp
+++ M03 part 2,1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
 
+int main() {
+    std::set<std::string> uniqueWords;
+    std::string word;
+    std::ofstream inputfile ("../data.txt");
+    if (inputfile.is_open()) {
+        std::cerr << "Error opening the file.\n";
+        return 1;
+    }
+
+    while (inputfile.is_open()) {
+        // Convert the word to lowercase to consider case-insensitive uniqueness
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+        // Check if the word is unique and add it to the set
+        if (uniqueWords.find(word) == uniqueWords.end()) {
+            uniqueWords.insert(word);
+        }
+    }
+
+    inputfile.close();
+
+    // Display unique words in ascending order
+    std::cout << "Unique words in ascending order:\n";
+    std::cout << "../data.txt\n";
+    return 0;
+}
